aa3136eb6a07c2ff7e9135e7f20b48f150da7517
Daniel Barton <djobarton@gmail.com>
2017-10-13 16:40:21 +0000

    add devise and category

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
   end
+  def math
+    @questions = Question.all.where(category: "Math" )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#home' 
   
+  get '/math' => 'pages#math'
+  
   resources :questions do
    resources :answers
   end
